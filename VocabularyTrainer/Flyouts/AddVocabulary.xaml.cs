a81caddf6b8bd1651c7864c66a24796bcf5b468a
becelot <benedict.becker93@gmail.com>
2015-01-11 16:43:12 +0000

    Adding vocabulary supported
    git-svn-id: https://sselab.de/lab2/private/svn/VocTrainer@15 5b5c503b-1cae-4e21-a3e4-c3aba52dd9a8

--- VocabularyTrainer/Flyouts/AddVocabulary.xaml.cs
+++ VocabularyTrainer/Flyouts/AddVocabulary.xaml.cs
@@ -37,13 +37,19 @@ namespace VocabularyTrainer.Flyouts
                     if (Config.Instance.categories.FindIndex( (x) => { return x.Equals(newCat); }) >= 0)
                     {
                         await Helper.MainWindow.ShowMessageAsync("Neue Kategorie hinzufügen", "Kategorie existiert berets!");
+                        comboCategory.Text = "";
                         return;
                     }
                     Config.Instance.categories.Add(result);
                     this.comboCategory.Items.Add(result);
                     this.comboCategory.SelectedIndex = this.comboCategory.Items.Count - 1;
+                } else
+                {
+                    //comboCategory.Text = "Empty";
                 }
             }
+            Logger.WriteLine(comboCategory.Text, "SlectionChanged");
+            canAddPropertyChanged(sender, null);
         }
 
         private async void comboLection_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -58,12 +64,44 @@ namespace VocabularyTrainer.Flyouts
                     if (Config.Instance.lections.FindIndex((x) => { return x.Equals(newLection); }) >= 0)
                     {
                         await Helper.MainWindow.ShowMessageAsync("Neue Lektion hinzufügen", "Lektion existiert berets!");
+                        comboLection.Text = "";
                         return;
                     }
                     Config.Instance.lections.Add(result);
                     this.comboLection.Items.Add(result);
                     this.comboLection.SelectedIndex = this.comboLection.Items.Count - 1;
+                } else
+                {
+                    comboLection.Text = "";
                 }
+            }
+            canAddPropertyChanged(sender, null);
+        }
+
+        private void buttonAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+            Vocabulary voc = new Vocabulary(this.textGerman.Text, this.textJap.Text, this.textRom.Text, this.comboCategory.Text, this.comboLection.Text);
+
+            if (VocabularyDatabase.Instance.vocs.FindIndex((x) => { return (x.german.Equals(voc.german)); }) < 0) {
+                VocabularyDatabase.Instance.vocs.Add(voc);
+                textGerman.Text = "";
+                textJap.Text = "";
+                textRom.Text = "";
+            } else
+            {
+                Helper.MainWindow.ShowMessageAsync("Vokabel existiert bereits", "Diese Vokabel ist bereits vorhanden");
+            }
+        }
+
+        private void canAddPropertyChanged(object sender, TextChangedEventArgs e)
+        {
+            if(textGerman.Text != "" && textJap.Text != "" && textRom.Text != "" && comboCategory.SelectedValue != null && !comboCategory.SelectedValue.Equals("") && comboLection.SelectedValue != null && !comboLection.SelectedValue.Equals(""))
+            {
+                buttonAdd.IsEnabled = true;
+            } else
+            {
+                buttonAdd.IsEnabled = false;
             }
         }
     }
